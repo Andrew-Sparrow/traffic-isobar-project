37f97e1f371d02fccf8aa85ebde10f0d0756c9e5
Andrew <8911102@gmail.com>
2021-11-30 16:16:16 +0500

    line-height: 80px was added ti footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -5,6 +5,7 @@ import Container from '../container/container';
 const FooterStyle = styled.div`
   background-color: ${props => props.color || props.theme.colors.secondary};
   min-height: 80px;
+  line-height: 80px;
 `;
 
 function Footer() {
