c0244c9f4d5d9ccf6ca13d7e3f63b3bb1c4f042b
Andrew <8911102@gmail.com>
2021-12-03 15:27:00 +0500

    text-align: justify;

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -6,18 +6,20 @@ const AboutStyle = styled.pre`
   padding-bottom: 40px;
   text-align: left;
   white-space: pre-line;
+
+  @media ${props => props.theme.media.phone} {
+    text-align: justify;
+  }
 `;
 
 function About() {
   return (
     <AboutStyle >
-      <p>
         At vero eos et accusamus et iusto odio dignissimos ducimus qui
         blanditiis praesentium voluptatum deleniti atque corrupti quos dolores
         et quas molestias excepturi sint occaecati cupiditate non provident,
         similique sunt in culpa qui officia deserunt mollitia animi,
         id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio.
-      </p>
     </AboutStyle>
   );
 }
